ed2925ff1d935b0a2a3197833f570e455d6e04bb
2overlord2 <126179310+2overlord2@users.noreply.github.com>
2023-05-12 11:07:03 -0300

    lampada ligou

---
+++ projeto/lamp.js
@@ -0,0 +1,13 @@
+const turnOn = document.getElementById('ligar');
+const turnOff = document. getElementById('desligar');
+const lamp = document.getElementById('lampada');
+function lampOn (){
+    lamp.src = './img/ligada.jpeg';
+}
+
+function lampOff (){
+    lamp.src = './img/desligada.jpeg';
+}
+
+turnOn.addEventListener('click', lampOn);
+turnOff.addEventListener('click', lampOff);
